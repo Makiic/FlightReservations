82e530fdbbd71efab93caf293b86187b78058d9f
Marija Ilic <ilicmarija1230@gmail.com>
2025-12-09 18:08:59 +0100

    Add .gitignore and remove unwanted files

---
+++ Backend/FlightReservations/DTO/LoginRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FlightReservations.DTO
+{
+    public class LoginRequestDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Backend/FlightReservations/Services/AuthService.cs
@@ -0,0 +1,36 @@
+﻿using FlightReservations.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace FlightReservations.Services
+{
+    public class AuthService
+    {
+        private readonly JwtKeyProvider _keyProvider;
+
+        public AuthService(JwtKeyProvider keyProvider)
+        {
+            _keyProvider = keyProvider;
+        }
+
+        public string GenerateJwtToken(User user)
+        {
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+            }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(_keyProvider.SigningKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
